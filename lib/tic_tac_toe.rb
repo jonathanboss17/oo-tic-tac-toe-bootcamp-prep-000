e01b0788c44d3b8b9d55fa59b23b58c4a469445d
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-15 10:12:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,7 +69,13 @@ class TicTacToe
     return count
   end
   
-  
+  def current_player(board)
+    if(turn_count(board) % 2 == 0)
+      return "X"
+    else
+      return "O"
+    end
+  end
 
 
 end 
